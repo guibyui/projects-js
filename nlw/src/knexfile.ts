99117716d24dfbe69b9fddda1bcd8b2555b8df58
guibyui <you@example.com>
2020-06-06 13:51:05 -0600

    tables created

---
+++ nlw/src/knexfile.ts
@@ -0,0 +1,23 @@
+import path from "path";
+
+module.exports = {
+  client: "sqlite3",
+  connection: {
+    filename: path.resolve(
+      __dirname,
+      "server",
+      "src",
+      "database",
+      "database.sqlite"
+    ),
+  },
+  migrations: {
+    directory: path.resolve(
+      __dirname,
+      "server",
+      "src",
+      "database",
+      "migrations"
+    ),
+  },
+};
