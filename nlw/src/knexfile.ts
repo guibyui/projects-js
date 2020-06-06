c5b3d9ddb55c528ded59123d5531d977e1aa1486
guibyui <you@example.com>
2020-06-06 14:37:56 -0600

    images added and working

--- nlw/src/knexfile.ts
+++ nlw/src/knexfile.ts
@@ -3,18 +3,13 @@ import path from "path";
 module.exports = {
   client: "sqlite3",
   connection: {
-    filename: path.resolve(
-      __dirname,
-      "database",
-      "database.sqlite"
-    ),
+    filename: path.resolve(__dirname, "database", "database.sqlite"),
   },
   migrations: {
-    directory: path.resolve(
-      __dirname,
-      "database",
-      "migrations"
-    )
+    directory: path.resolve(__dirname, "database", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "database", "seeds"),
   },
   useNullAsDefault: true,
 };
