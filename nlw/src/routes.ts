25b5ae796e235a8b7d1d57e6a1bb2da26a6e662d
guibyui <you@example.com>
2020-06-17 17:08:12 -0600

    gitignore added

--- nlw/src/routes.ts
+++ nlw/src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import multerConfig from "./config/multer";
+import { celebrate, Joi } from "celebrate";
 
 import points_controller from "./controllers/points_controller";
 import items_controller from "./controllers/items_controller";
@@ -18,8 +19,25 @@ routes.get("/items", itemsController.index);
 routes.get("/points/", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
-routes.post("/points", upload.single('image'), pointsController.create);
-
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      phoneNumber: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      state: Joi.string().required().max(2),
+      city: Joi.string().required(),
+      items: Joi.string().required(),
+    }),
+  }, {
+      abortEarly: false
+  }),
+  pointsController.create
+);
 
 // return response.json(items);
 
